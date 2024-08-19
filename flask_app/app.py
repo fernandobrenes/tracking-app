3b24eba027e9fb0c5bed0fd01a767454141cc7a5
Fernando Brenes <fernandobrenes@gmail.com>
2024-08-19 10:52:53 -0600

    App ready

---
+++ flask_app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from models import db, User
+from forms import LoginForm
+import config
+
+app = Flask(__name__)
+app.config.from_object(config.Config)
+
+db.init_app(app)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html', form=form)
+
+
+@app.route('/home')
+def home():
+    return 'Welcome to the home page!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
